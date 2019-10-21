404c3ff3a28fd455e0e83d386d6910592636ca74
Rodrigo Silveira <rodrixar@gmail.com>
2019-10-21 14:53:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name='FormigoneSyllabify',
     version='v0.0.1',
     packages=['formigone', ],
+    license='MIT',
+    author='Rodrigo Silveira',
+    author_email='webmaster@rodrigo-silveira.com',
+    url='https://github.com/formigone/syllabify',
 )
